fdf47e420de29272c4d3afa60472a7f81a108671
eric valcik <eric.valcik@gmail.com>
2023-08-04 10:08:12 +0200

    Add suit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-       <Image src={'/suit-turned-away.png'} width={500} height={500} priority />
-      <Image src={'/suit-turned-away.png'} width={500} height={500} priority />
+      <div className="group cursor-pointer">
+        <Image
+          src={"/suit-turned-away.png"}
+          alt={"Suit turned away"}
+          width={500}
+          height={500}
+          priority
+          className="group-hover:hidden"
+        />
+        <Image
+          src={"/suit-turning-to-you.png"}
+          alt={"Suit turning to you"}
+          width={500}
+          height={500}
+          priority
+          className="hidden group-hover:block"
+        />
+      </div>
     </main>
-  )
+  );
 }
